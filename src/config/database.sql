431b7ef53b0167b56b5ceeb8ad7cabad2bfff648
Sanjero20 <gelosantos72@gmail.com>
2024-05-24 19:55:02 +0800

    change username to email column

--- src/config/database.sql
+++ src/config/database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE job_tracker;
 CREATE TABLE accounts
 (
   user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 );
 
